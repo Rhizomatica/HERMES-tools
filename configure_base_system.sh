d1da3319d49a3b19246ac895362fc1f3dc73a5ae
Rafael Diniz <rafael@riseup.net>
2020-06-18 01:05:48 -0300

    adding for info

--- configure_base_system.sh
+++ configure_base_system.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 
+# uucp + mozjpeg
 wget http://www.telemidia.puc-rio.br/~rafaeldiniz/public_files/hermes-repo/rafaeldiniz.gpg.key
 apt-key add rafaeldiniz.gpg.key
 echo deb http://www.telemidia.puc-rio.br/~rafaeldiniz/public_files/hermes-repo/ buster main > /etc/apt/sources.list.d/hermes.conf
 apt-get update
-apt-get -y install apache2 uucp php imagemagick gnupg seccure mozjpeg
+apt-get -y install  git apache2 uucp php imagemagick gnupg seccure mozjpeg
 
+# todo: make package
+git clone https://github.com/DigitalHERMES/ardopc.git
+cd ardop2ofdm
+make
+make install
 
-# install mozjpeg: https://github.com/mozilla/mozjpeg.git
+# todo: make package
+git clone https://github.com/DigitalHERMES/rhizo-uuardop.git
+cd rhizo-uuardop
+make
+make install
 
+rm /var/www/html/index.html
 
-# TODO...
+# put config files in place!
+
+systemctl daemon-reload
+systemctl enable ardop
+systemctl enable uuardopd
