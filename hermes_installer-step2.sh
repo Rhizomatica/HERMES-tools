2f40b0bd826b4d8e314523b77d68c4e204b120d5
Rafael Diniz <rafael@riseup.net>
2023-09-27 12:06:15 +0100

    added installed step2

---
+++ hermes_installer-step2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+INSTALLER_DIR="/root"
+
+cd ${INSTALLER_DIR}
+tar xvf hermes-installer.tar.gz
+cd hermes-installer
+echo -n "Type the station name: "
+read station_name
+
+echo "Running the installer for ${station_name}"
+
+./installer.sh ${station_name}
+
+echo "Installation finished. Disabling the installer."
+
+systemctl disable installer
