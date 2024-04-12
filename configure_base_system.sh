8f70fbfac64f19754e775be1c66eadcb0b0db9d8
Rafael Diniz <rafael@riseup.net>
2024-04-12 15:59:11 +0100

    advancing

--- configure_base_system.sh
+++ configure_base_system.sh
@@ -1,4 +1,32 @@
-#!/bin/sh
+#!/bin/bash
+
+DIRECTORY=/root/system
+
+let i=0 # define counting variable
+W=() # define working array
+while read -r line; do # process file by file
+    let i=$i+1
+    W+=($i "$line")
+done < <( ls -1 ${DIRECTORY} )
+FILE=$(dialog --title "List stations for installation:" --menu "Chose one" 24 80 17 "${W[@]}" 3>&2 2>&1 1>&3) # show dialog and store output
+RESULT=$?
+clear
+if [ $RESULT -eq 0 ]; then # Exit with OK
+     echo "${W[$((FILE * 2 -1))]}"
+fi
+
+dialog --yesno "Start recoding the image?" 6 25
+RESULT=$?
+if [[ $RESULT -eq 1 ]]; then
+     exit
+fi
+
+exit
+
+clear
+echo "/home/$CHOICE"
+
+exit
 
 # uucp + mozjpeg
 wget http://www.telemidia.puc-rio.br/~rafaeldiniz/public_files/hermes-repo/rafaeldiniz.gpg.key
