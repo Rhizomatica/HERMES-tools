3a5b2e979805999b72b41731603e7b495e790977
Rafael Diniz <rafael@riseup.net>
2025-10-15 00:44:00 +0100

    image_installer getting ready

--- image_installer.sh
+++ image_installer.sh
@@ -35,6 +35,18 @@ cleanup() {
 }
 trap cleanup EXIT
 
+# check if image size is less than 16GB
+IMG_SIZE=$(stat -c%s "$IMG")
+if [ "$IMG_SIZE" -lt 17179869184 ]; then
+    echo "[*] Resizing image..."
+    qemu-img resize "${IMG}" 17179869184
+    parted --script "$IMG" \
+           resizepart 2 100% \
+        && e2fsck -f "${IMG}p2" \
+        && resize2fs "${IMG}p2"
+else
+    echo "[*] Image size is already 16GB or larger, skipping resize."
+fi
 
 LOOP=$(losetup --show -fP "$IMG")
 BOOT_PART="${LOOP}p1"
@@ -48,80 +60,31 @@ cp $INSTALLER "$WORKDIR/root/"
 systemd-nspawn -D "$WORKDIR" /bin/bash -c "\
   ls -l; tar xzf /root/$(basename $INSTALLER) -C /root/ && \
   cd /root/hermes-installer && \
-  ./installer.sh $FQDN && \
-  rm -rf /root/hermes-installer /root/$(basename $INSTALLER)"
+  ./installer.sh -v $FQDN"
 
-exit
+echo "[*] Done. systemd-spawn is off."
 
-KERNEL="$WORKDIR/kernel8.img"
-DTB="$WORKDIR/bcm2711-rpi-4-b.dtb"
+MNT_BOOT="$WORKDIR/boot/firmware"
 
-cp "$WORKDIR/boot/kernel8.img" "$KERNEL"
-cp "$WORKDIR/boot/bcm2711-rpi-4-b.dtb" "$DTB"
+echo "[*] Copying kernel and DTB..."
+mkdir -p boot
+cp "$MNT_BOOT/kernel8.img" boot/
+cp "$MNT_BOOT/bcm2711-rpi-4-b.dtb" boot/
 
-# Get root UUID
-ROOT_UUID=$(lsblk -no UUID "$ROOT_PART")
+echo "[*] Unmounting image..."
+sudo umount "$MNT_BOOT"
+sudo umount "$WORKDIR"
+sudo losetup -d "$LOOP"
 
-umount "$WORKDIR/boot"
-losetup -d "$LOOP"
+echo "[*] Booting QEMU for last step setup..."
+qemu-system-aarch64 \
+    -M raspi4b \
+    -m 2G \
+    -smp 4 \
+    -kernel "boot/kernel8.img" \
+    -dtb "boot/bcm2711-rpi-4-b.dtb" \
+    -append "rw earlyprintk loglevel=8 root=/dev/mmcblk1p2 rootwait" \
+    -drive "file=$IMG,if=sd,format=raw" \
+    -display gtk
 
-echo "Extracted kernel: $KERNEL"
-echo "Extracted dtb:    $DTB"
-
-# --- Boot QEMU (Raspberry Pi 4 with USB networking) ---
-NET_OPTS="-netdev user,id=net0,hostfwd=tcp::2222-:22 -device usb-net,netdev=net0"
-
-
-$QEMU_BIN -machine virt -cpu cortex-a72 -smp 6 -m 4G -nographic \
-    -kernel kernel/vmlinuz -initrd kernel/initrd.gz -append "root=/dev/vda2 rootfstype=ext4 rw panic=0 console=ttyAMA0,115200 rootwait=1" \
-    -drive format=raw,file=${IMG},if=none,id=hd0,cache=writeback \
-    -accel tcg,thread=multi \
-    -device virtio-blk,drive=hd0,bootindex=0 \
-    -netdev user,id=mynet,hostfwd=tcp::2222-:22 \
-    -device virtio-net-pci,netdev=mynet \
-    -monitor telnet:127.0.0.1:5555,server,nowait
-
-
-# -M raspi4b \
-#  -kernel "$KERNEL" -dtb "$DTB" \
-#  -m 2048 \
-#  -drive file="$IMG",format=raw,if=sd \
-#  -append "rw console=ttyAMA0,115200 root=/dev/mmcblk1p2 rootfstype=ext4 rootwait" \
-#  -device usb-kbd \
-#  $NET_OPTS \
-#  -serial stdio \
-#  -graphic gtk
-
-  # -serial telnet:localhost:4321,server,nowait \
- # -monitor telnet:localhost:4322,server,nowait \
- # -nographic
-
-#-display none &
-#QEMU_PID=$!
-
-#  -serial mon:stdio \
-#    -serial telnet:localhost:4321,server,nowait \
-#  -append "rw console=ttyAMA0,115200 root=/dev/mmcblk0p2 rootfstype=ext4 rootwait" \
-#  -display none &
-#   -append "rw root=UUID=7c32fc47-9afe-48a1-8b32-00cf57bc60de rootfstype=ext4 rootwait" \
-
-echo "QEMU started. Waiting for SSH on port 2222..."
-
-# --- Wait for SSH to come up ---
-until sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no -p 2222 "$USER"@127.0.0.1 "echo ssh-up" 2>/dev/null; do
-  sleep 5
-done
-
-echo "SSH is up. Copying installer..."
-sshpass -p "$PASS" scp -P 2222 -o StrictHostKeyChecking=no "$INSTALLER" "$USER"@127.0.0.1:/home/$USER/
-
-echo "Running installer inside guest..."
-sshpass -p "$PASS" ssh -p 2222 -o StrictHostKeyChecking=no "$USER"@127.0.0.1 "\
-  tar xzf hermes-installer.tar.gz && \
-  cd hermes-installer && \
-  ./installer.sh $FQDN"
-
-echo "Rebooting guest..."
-sshpass -p "$PASS" ssh -p 2222 -o StrictHostKeyChecking=no "$USER"@127.0.0.1 "sudo reboot"
-
-echo "Done. VM is rebooting."
+echo "Done. Image $IMG is ready!"
