0450d1895988317aac245207ef35798bdf51e435
Rafael Diniz <rafael@riseup.net>
2025-10-15 01:12:15 +0100

    image_installer.sh works

--- image_installer.sh
+++ image_installer.sh
@@ -3,17 +3,20 @@
 # HERMES Project
 # License: GPLv3
 #
-# Usage: sudo ./image_installer.sh  raspios.img hermes-installer.tar.gz station.hermes.radio [user] [pass]
+# Usage: sudo ./image_installer.sh raspios.img hermes-installer.tar.gz station.hermes.radio
 
 set -euo pipefail
 
-#if [ "$EUID" -ne 0 ]; then
-#  echo "Please run as root (for loop device setup)"
-#  exit 1
-#fi
+DEFAULT_IMG_NAME="2025-10-01-raspios-bookworm-arm64-lite.img"
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root (for loop device setup)"
+  exit 1
+fi
 
 if [ "$#" -lt 3 ]; then
-  echo "Usage: sudo $0 <image> <installer.tar.gz> <FQDN> [user] [pass]"
+  echo "Usage: sudo $0 <image> <installer.tar.gz>"
+  echo "<image> - path to Raspberry Pi OS image or \"auto\" to download automatically"
   exit 1
 fi
 
@@ -30,27 +33,47 @@ cleanup() {
   echo "Cleaning up..."
   umount "$WORKDIR/boot/firmware" 2>/dev/null || true
   umount "$WORKDIR" 2>/dev/null || true
-  losetup -d "$LOOP" 2>/dev/null || true
   rm -rf "$WORKDIR"
+  losetup -d "$LOOP" 2>/dev/null || true
 }
 trap cleanup EXIT
+
+if [ "$IMG" == "auto" ]; then
+    echo "[*] Downloading latest Raspberry Pi OS Lite image..."
+    if [ -f "${DEFAULT_IMG_NAME}.xz" ]; then
+        rm -f "${DEFAULT_IMG_NAME}"
+        unxz -k "${DEFAULT_IMG_NAME}.xz"
+        echo "[*] Using cached image at $IMG.xz"
+    else
+        wget https://downloads.raspberrypi.com/raspios_oldstable_lite_arm64/images/raspios_oldstable_lite_arm64-2025-10-02/${DEFAULT_IMG_NAME}.xz
+        rm -f "${DEFAULT_IMG_NAME}"
+        unxz -k "${DEFAULT_IMG_NAME}.xz"
+        echo "[*] Downloaded image to $IMG.xz"
+    fi
+    IMG="${FQDN}.img"
+    mv "${DEFAULT_IMG_NAME}" "${IMG}"
+fi
 
 # check if image size is less than 16GB
 IMG_SIZE=$(stat -c%s "$IMG")
 if [ "$IMG_SIZE" -lt 17179869184 ]; then
-    echo "[*] Resizing image..."
+    echo "[*] Resizing image to 16GB..."
     qemu-img resize "${IMG}" 17179869184
-    parted --script "$IMG" \
+
+    LOOP=$(losetup --show -fP "$IMG")
+    BOOT_PART="${LOOP}p1"
+    ROOT_PART="${LOOP}p2"
+
+    parted --script "$LOOP" \
            resizepart 2 100% \
-        && e2fsck -f "${IMG}p2" \
-        && resize2fs "${IMG}p2"
+        && e2fsck -f "${ROOT_PART}" \
+        && resize2fs "${ROOT_PART}"
 else
+    LOOP=$(losetup --show -fP "$IMG")
+    BOOT_PART="${LOOP}p1"
+    ROOT_PART="${LOOP}p2"
     echo "[*] Image size is already 16GB or larger, skipping resize."
 fi
-
-LOOP=$(losetup --show -fP "$IMG")
-BOOT_PART="${LOOP}p1"
-ROOT_PART="${LOOP}p2"
 
 mount "$ROOT_PART" "$WORKDIR"
 mount "$BOOT_PART" "$WORKDIR/boot/firmware"
@@ -87,4 +110,4 @@ qemu-system-aarch64 \
     -drive "file=$IMG,if=sd,format=raw" \
     -display gtk
 
-echo "Done. Image $IMG is ready!"
+echo "[*] Done. Image $IMG is ready!"
